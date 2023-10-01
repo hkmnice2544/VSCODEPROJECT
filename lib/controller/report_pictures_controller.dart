5d0021e99273d4724a66f4b7b4adce0c42da2a46
hkmnice2544 <Hatsaya13759@gmail.com>
2023-10-01 13:43:21 +0700

    ADD report picturs

---
+++ lib/controller/report_pictures_controller.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutterr/constant/constant_value.dart';
+import 'package:flutterr/model/Report_pictures_Model.dart';
+import 'package:http/http.dart' as http;
+
+class Report_PicturesController {
+  Report_pictures? report_pictures;
+
+  static Future<List<Report_pictures>> saveInform_Pictures(
+      List<Map<String, dynamic>> dataList) async {
+    final url = Uri.parse('$baseURL/report_pictures/addReport_pictures');
+
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: jsonEncode(dataList),
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonResponse = json.decode(response.body);
+      final List<Report_pictures> savedReport_picturesList = jsonResponse
+          .map(
+              (dynamic item) => Report_pictures.fromJsonToReport_pictures(item))
+          .toList();
+
+      return savedReport_picturesList;
+    } else {
+      throw Exception('Failed to save savedInform_PicturesList');
+    }
+  }
+}
---
+++ lib/model/Report_pictures_Model.dart
@@ -0,0 +1,33 @@
+import 'package:flutterr/model/Report_Model.dart';
+
+class Report_pictures {
+  int? reportpictures_id;
+  String? picture_url;
+  ReportRepair? reportrepair;
+
+  Report_pictures({
+    this.reportpictures_id,
+    this.picture_url,
+    this.reportrepair,
+  });
+  factory Report_pictures.fromJsonToReport_pictures(Map<String, dynamic> json) {
+    final reportrepairJson = json['reportrepair'] as Map<String, dynamic>;
+
+    final reportrepair = ReportRepair.fromJsonToReportRepair(reportrepairJson);
+
+    final inform_Pictures = Report_pictures(
+        reportpictures_id: json['reportpictures_id'] as int,
+        picture_url: json['picture_url'],
+        reportrepair: reportrepair);
+
+    return inform_Pictures;
+  }
+
+  Map<String, dynamic> fromReport_picturesToJson() {
+    return <String, dynamic>{
+      'reportpictures_id': reportpictures_id,
+      'picture_url': picture_url,
+      'informRepairDetails': reportrepair?.fromReportRepairToJson(),
+    };
+  }
+}
